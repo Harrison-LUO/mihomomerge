54c1eb70221c461077fb4cf59dcacd0d813255e1
p0w0q <87455543+fortressme@users.noreply.github.com>
2025-07-15 10:44:34 +0000

    change timeout

--- ROUTER.js
+++ ROUTER.js
@@ -71,7 +71,7 @@ function main(config, profileName) {
     // 修改订阅组选项
     updateGroupOption(config, "type", ["load-balance", "fallback", "url-test"], "lazy", true);
     updateGroupOption(config, "type", ["load-balance", "fallback", "url-test"], "max-failed-times", 7);
-    updateGroupOption(config, "type", ["load-balance", "fallback", "url-test"], "timeout", 20000);
+    updateGroupOption(config, "type", ["load-balance", "fallback", "url-test"], "timeout", 15000);
 
     // 修改节点skip-cert-verify选项
     updateProxyOption(config, "type", ["vmess", "vless", "trojan", "ss", "hysteria2", "tuic"], "skip-cert-verify", true);
